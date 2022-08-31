11ee2c489415a9134bc45098a89758c6834d39df
Lars Mitsem Selbekk <lars@selbekk.com>
2022-08-31 19:28:05 +0200

    Add project drag-and-drop easter egg

--- src/components/FrontPage/index.tsx
+++ src/components/FrontPage/index.tsx
@@ -1,19 +1,58 @@
-import ProjectOverview from "../ProjectOverview";
-import React from "react";
-import styles from "./FrontPage.module.css";
+import ProjectOverview from "../ProjectOverview"
+import React, { useState } from "react"
+import styles from "./FrontPage.module.css"
+import CursedLars from "../../../public/Cursed_Lars.jpg"
 
 export const FrontPage: React.FC = () => {
+  const [isDropping, setIsDropping] = useState(false)
+
+  const onDragOver: React.DragEventHandler = (event) => {
+    if (event.dataTransfer.effectAllowed === "copyLink") {
+      setIsDropping(true)
+      event.preventDefault()
+    }
+  }
+
+  const onDragLeave: React.DragEventHandler = (event) => {
+    setIsDropping(false)
+    event.preventDefault()
+  }
+
+  const onDrop: React.DragEventHandler = (event) => {
+    if (event.dataTransfer.effectAllowed === "copyLink") {
+      const url = event.dataTransfer
+        .getData("text/uri-list")
+        .split("\n\r")
+        .filter((line) => (line.at(0) ?? "#") !== "#")
+        .at(0)
+
+      if (url) {
+        setIsDropping(false)
+        window.location.href = url
+      }
+    }
+  }
+
   return (
     <>
       <h1>Lars Mitsem Selbekk</h1>
-      <div className={styles.portraitWrapper}>
+      <div
+        className={styles.portraitWrapper}
+        onDragOver={onDragOver}
+        onDragLeave={onDragLeave}
+        onDrop={onDrop}
+      >
         <img
-          src="https://avatars.githubusercontent.com/u/6224384?v=4"
+          src={
+            isDropping
+              ? CursedLars
+              : "https://avatars.githubusercontent.com/u/6224384?v=4"
+          }
           alt="A portrait picture of Lars"
           className={styles.portrait}
         />
       </div>
       <ProjectOverview />
     </>
-  );
-};
+  )
+}
--- src/components/ProjectDisplay/index.tsx
+++ src/components/ProjectDisplay/index.tsx
@@ -1,23 +1,37 @@
-import React from "react"
-import {Project} from "../ProjectOverview"
+import React, { DragEventHandler } from "react"
+import { Project } from "../ProjectOverview"
 import styles from "./ProjectDisplay.module.css"
-import useIsDarkMode from "../../hooks/useIsDarkMode";
+import useIsDarkMode from "../../hooks/useIsDarkMode"
 
 export interface ProjectDisplayProps {
-    project: Project;
+  project: Project
 }
 
-const ProjectDisplay: React.FC<ProjectDisplayProps> = ({project}) => {
-    const isDarkMode = useIsDarkMode()
-    return (
-        <a href={project.path} className={styles.ProjectDisplay}>
-            <figure className={styles.ProjectDisplayContent}>
-                <img src={isDarkMode ? project.darkIcon : project.lightIcon} alt={project.name}/>
-                <figcaption>{project.name}</figcaption>
-            </figure>
-        </a>
-    )
-}
+const ProjectDisplay: React.FC<ProjectDisplayProps> = ({ project }) => {
+  const isDarkMode = useIsDarkMode()
 
+  const onDragStart: DragEventHandler<HTMLAnchorElement> = (event) => {
+    event.dataTransfer.setData("text/uri-list", project.path)
+    event.dataTransfer.effectAllowed = "copyLink"
+  }
+
+  return (
+    <a
+      href={project.path}
+      className={styles.ProjectDisplay}
+      draggable={true}
+      onDragStart={onDragStart}
+    >
+      <figure className={styles.ProjectDisplayContent} draggable={false}>
+        <img
+          src={isDarkMode ? project.darkIcon : project.lightIcon}
+          alt={project.name}
+          draggable={false}
+        />
+        <figcaption>{project.name}</figcaption>
+      </figure>
+    </a>
+  )
+}
 
 export default ProjectDisplay
